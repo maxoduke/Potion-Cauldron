e2f199f1fdaea3a089854d174ee4c75149d6c9d4
Maxoduke <maxoduke@outlook.com>
2023-11-06 05:26:12 +0530

    Port to 1.20.2

--- forge/src/main/java/dev/maxoduke/mods/potioncauldron/networking/NetworkHandler.java
+++ forge/src/main/java/dev/maxoduke/mods/potioncauldron/networking/NetworkHandler.java
@@ -6,38 +6,40 @@ import dev.maxoduke.mods.potioncauldron.networking.packets.ParticlePacket;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.fml.DistExecutor;
-import net.minecraftforge.network.NetworkRegistry;
-import net.minecraftforge.network.simple.SimpleChannel;
+import net.minecraftforge.network.Channel;
+import net.minecraftforge.network.ChannelBuilder;
+import net.minecraftforge.network.NetworkDirection;
+import net.minecraftforge.network.SimpleChannel;
 
 public class NetworkHandler
 {
-    private static final String PROTOCOL_VERSION = "1";
+    private static final int PROTOCOL_VERSION = 1;
     public static final SimpleChannel INSTANCE;
 
     static
     {
-        INSTANCE = NetworkRegistry.newSimpleChannel(
-            new ResourceLocation(PotionCauldron.MOD_ID, "main"),
-            () -> PROTOCOL_VERSION, PROTOCOL_VERSION::equals, PROTOCOL_VERSION::equals
-        );
+        INSTANCE = ChannelBuilder
+            .named(new ResourceLocation(PotionCauldron.MOD_ID, "main"))
+            .networkProtocolVersion(PROTOCOL_VERSION)
+            .clientAcceptedVersions(Channel.VersionTest.exact(PROTOCOL_VERSION))
+            .serverAcceptedVersions(Channel.VersionTest.exact(PROTOCOL_VERSION))
+            .simpleChannel();
     }
 
     public static void register()
     {
-        INSTANCE.registerMessage(1, ClientConfig.class, ClientConfig::writeToBuf, ClientConfig::fromBuf, (config, contextSupplier) ->
-        {
-            contextSupplier.get().enqueueWork(() ->
-                DistExecutor.unsafeRunWhenOn(Dist.CLIENT, () -> () -> ClientNetworking.receiveConfigFromServer(config))
-            );
-            contextSupplier.get().setPacketHandled(true);
-        });
+        INSTANCE
+            .messageBuilder(ClientConfig.class, 1, NetworkDirection.PLAY_TO_CLIENT)
+            .encoder(ClientConfig::writeToBuf)
+            .decoder(ClientConfig::fromBuf)
+            .consumerMainThread((config, context) -> DistExecutor.unsafeRunWhenOn(Dist.CLIENT, () -> () -> ClientNetworking.receiveConfigFromServer(config)))
+            .add();
 
-        INSTANCE.registerMessage(2, ParticlePacket.class, ParticlePacket::writeToBuf, ParticlePacket::fromBuf, (packet, contextSupplier) ->
-        {
-            contextSupplier.get().enqueueWork(() ->
-                DistExecutor.unsafeRunWhenOn(Dist.CLIENT, () -> () -> ClientNetworking.receiveParticlesFromServer(packet))
-            );
-            contextSupplier.get().setPacketHandled(true);
-        });
+        INSTANCE
+            .messageBuilder(ParticlePacket.class, 2, NetworkDirection.PLAY_TO_CLIENT)
+            .encoder(ParticlePacket::writeToBuf)
+            .decoder(ParticlePacket::fromBuf)
+            .consumerMainThread((packet, context) -> DistExecutor.unsafeRunWhenOn(Dist.CLIENT, () -> () -> ClientNetworking.receiveParticlesFromServer(packet)))
+            .add();
     }
 }
--- forge/src/main/java/dev/maxoduke/mods/potioncauldron/platform/ForgeNetworkHelper.java
+++ forge/src/main/java/dev/maxoduke/mods/potioncauldron/platform/ForgeNetworkHelper.java
@@ -5,17 +5,17 @@ import dev.maxoduke.mods.potioncauldron.networking.NetworkHandler;
 import dev.maxoduke.mods.potioncauldron.networking.packets.ParticlePacket;
 import dev.maxoduke.mods.potioncauldron.platform.services.INetworkHelper;
 import net.minecraft.server.level.ServerPlayer;
-import net.minecraftforge.network.NetworkDirection;
+import net.minecraftforge.network.PacketDistributor;
 
 public class ForgeNetworkHelper implements INetworkHelper
 {
     public void sendConfigToClient(ServerPlayer player, ClientConfig config)
     {
-        NetworkHandler.INSTANCE.sendTo(config, player.connection.connection, NetworkDirection.PLAY_TO_CLIENT);
+        NetworkHandler.INSTANCE.send(config, PacketDistributor.PLAYER.with(player));
     }
 
     public void sendParticlesToClient(ServerPlayer player, ParticlePacket particlePacket)
     {
-        NetworkHandler.INSTANCE.sendTo(particlePacket, player.connection.connection, NetworkDirection.PLAY_TO_CLIENT);
+        NetworkHandler.INSTANCE.send(particlePacket, PacketDistributor.PLAYER.with(player));
     }
 }
