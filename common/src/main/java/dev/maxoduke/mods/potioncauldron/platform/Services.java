9add74bb83b715f7dba9798036ae2822f9d1f597
Maxoduke <maxoduke@outlook.com>
2023-11-08 13:42:55 +0530

    Fix typo and formatting

--- common/src/main/java/dev/maxoduke/mods/potioncauldron/platform/Services.java
+++ common/src/main/java/dev/maxoduke/mods/potioncauldron/platform/Services.java
@@ -6,17 +6,17 @@ import dev.maxoduke.mods.potioncauldron.platform.services.IPlatformHelper;
 
 import java.util.ServiceLoader;
 
-public class Services 
+public class Services
 {
     public static final IPlatformHelper PLATFORM = load(IPlatformHelper.class);
     public static final INetworkHelper NETWORK = load(INetworkHelper.class);
 
-    public static <T> T load(Class<T> clazz) {
-
+    public static <T> T load(Class<T> clazz)
+    {
         final T loadedService = ServiceLoader.load(clazz)
-                .findFirst()
-                .orElseThrow(() -> new NullPointerException("Failed to load service for " + clazz.getName()));
-        
+            .findFirst()
+            .orElseThrow(() -> new NullPointerException("Failed to load service for " + clazz.getName()));
+
         PotionCauldron.LOG.debug("Loaded {} for service {}", loadedService, clazz);
         return loadedService;
     }
